8f17b9c9f0343438c7e79e8c1de46a4ee657518e
rodgeraraujo <rogerio.araujo@mail.com>
2019-09-25 20:37:42 -0300

    :sparkles: create simple spa crud, consuming a rest api

---
+++ client/src/components/Books.vue
@@ -0,0 +1,262 @@
+<template>
+  <div class="container">
+    
+    <div class="row">
+      <div class="col-sm-10">
+        <!-- <navbar :title=title></!-->
+        <h1>Books</h1>
+        <hr><br><br>
+        <alert :message=message v-if="showMessage"></alert>
+        <button type="button" class="btn btn-success btn-sm" v-b-modal.book-modal>Add Livro</button>
+        <br><br>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Título</th>
+              <th scope="col">Autor</th>
+              <th scope="col">Lido?</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(book, index) in books" :key="index">
+              <td>{{ book.title }}</td>
+              <td>{{ book.author }}</td>
+              <td>
+                <span v-if="book.read">Sim</span>
+                <span v-else>Não</span>
+              </td>
+              <td>
+                <div class="btn-group" role="group">
+                  <button
+                          type="button"
+                          class="btn btn-warning btn-sm"
+                          v-b-modal.book-update-modal
+                          @click="editBook(book)">
+                      Atualizar
+                  </button>
+                  <button
+                          type="button"
+                          class="btn btn-danger btn-sm"
+                          @click="onDeleteBook(book)">
+                      Deletar
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <b-modal ref="addBookModal"
+            id="book-modal"
+            title="Add um novo livro"
+            hide-footer>
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+      <b-form-group id="form-title-group"
+                    label="Título:"
+                    label-for="form-title-input">
+          <b-form-input id="form-title-input"
+                        type="text"
+                        v-model="addBookForm.title"
+                        required
+                        placeholder="Título">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-author-group"
+                      label="Autor:"
+                      label-for="form-author-input">
+            <b-form-input id="form-author-input"
+                          type="text"
+                          v-model="addBookForm.author"
+                          required
+                          placeholder="Autor">
+            </b-form-input>
+          </b-form-group>
+        <b-form-group id="form-read-group">
+          <b-form-checkbox-group v-model="addBookForm.read" id="form-checks">
+            <b-form-checkbox value="true">Lido?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Salvar</b-button>
+          <b-button type="reset" variant="danger">Resetar</b-button>
+        </b-button-group>
+      </b-form>
+    </b-modal>
+    <b-modal ref="editBookModal"
+            id="book-update-modal"
+            title="Atualizar"
+            hide-footer>
+      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+      <b-form-group id="form-title-edit-group"
+                    label="Título:"
+                    label-for="form-title-edit-input">
+          <b-form-input id="form-title-edit-input"
+                        type="text"
+                        v-model="editForm.title"
+                        required
+                        placeholder="Título">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-author-edit-group"
+                      label="Autor:"
+                      label-for="form-author-edit-input">
+            <b-form-input id="form-author-edit-input"
+                          type="text"
+                          v-model="editForm.author"
+                          required
+                          placeholder="Autor">
+            </b-form-input>
+          </b-form-group>
+        <b-form-group id="form-read-edit-group">
+          <b-form-checkbox-group v-model="editForm.read" id="form-checks">
+            <b-form-checkbox value="true">Lido?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Atualizar</b-button>
+          <b-button type="reset" variant="danger">Cancelar</b-button>
+        </b-button-group>
+      </b-form>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Alert from './Alert.vue';
+import Navbar from './Navbar.vue';
+
+const API_URL = 'http://localhost:5000';
+
+export default {
+  data() {
+    return {
+      books: [],
+      addBookForm: {
+        title: '',
+        author: '',
+        read: [],
+      },
+      message: '',
+      showMessage: false,
+      editForm: {
+        id: '',
+        title: '',
+        author: '',
+        read: [],
+      },
+    };
+  },
+  components: {
+    alert: Alert,
+    navbar: Navbar
+  },
+  methods: {
+    getBooks() {
+      axios.get(API_URL + '/books')
+        .then((res) => {
+          this.books = res.data.books;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+    addBook(payload) {
+      axios.post(API_URL + '/books', payload)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Livro adicionado!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+          this.getBooks();
+        });
+    },
+    initForm() {
+      this.addBookForm.title = '';
+      this.addBookForm.author = '';
+      this.addBookForm.read = [];
+      this.editForm.id = '';
+      this.editForm.title = '';
+      this.editForm.author = '';
+      this.editForm.read = [];
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addBookModal.hide();
+      let read = false;
+      if (this.addBookForm.read[0]) read = true;
+      const payload = {
+        title: this.addBookForm.title,
+        author: this.addBookForm.author,
+        read, // property shorthand
+      };
+      this.addBook(payload);
+      this.initForm();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.$refs.addBookModal.hide();
+      this.initForm();
+    },
+    editBook(book) {
+      this.editForm = book;
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      let read = false;
+      if (this.editForm.read[0]) read = true;
+      const payload = {
+        title: this.editForm.title,
+        author: this.editForm.author,
+        read,
+      };
+      this.updateBook(payload, this.editForm.id);
+    },
+    updateBook(payload, bookID) {
+      axios.put(API_URL + `books/${bookID}`, payload)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Livro atualizado!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      this.initForm();
+      this.getBooks(); // why?
+    },
+    removeBook(bookID) {
+      axios.delete(API_URL + `/books/${bookID}`)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Livro removido!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    onDeleteBook(book) {
+      this.removeBook(book.id);
+    },
+  },
+  created() {
+    this.getBooks();
+  },
+};
+</script>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">Books</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item href="/ping">Ping</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+}
+</script>
